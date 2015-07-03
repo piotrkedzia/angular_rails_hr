bd6214e78fe2103b8a6b093375069cb18e3dca08
Piotr Kędzia <piotr@kedzia.eu>
2015-07-03 19:51:58 +0200

    Add customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,12 @@
 class Customer < ActiveRecord::Base
   has_many :orders
 
-  validates :first_name, :last_name, :email, :address, :zip_code, presence: true
-  validates :discount, numericality: { less_than: 1 }
+  VALID_PHONE_NUMBER_REGEX = %r{\A[+]?\d+(?>[- .]\d+)*\z}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  validates :first_name, :last_name, :email, :address, :zip_code, presence: true
+  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
+  validates :email, format:  { with: VALID_EMAIL_REGEX, message: 'must be valid' }
+  validates :phone_number, :fax_number, format: { with: VALID_PHONE_NUMBER_REGEX, message: 'must be valid' }
+  
 end
