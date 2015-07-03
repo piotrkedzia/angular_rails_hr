195e77f2dc9988435fdf1f586eb434e390a3caa8
Piotr Kędzia <piotr@kedzia.eu>
2015-07-03 23:35:28 +0200

    Allow blank phone number in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,5 @@ class Customer < ActiveRecord::Base
   validates :first_name, :last_name, :email, :address, :zip_code, presence: true
   validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
   validates :email, format:  { with: VALID_EMAIL_REGEX, message: 'must be valid' }
-  validates :phone_number, :fax_number, format: { with: VALID_PHONE_NUMBER_REGEX, message: 'must be valid' }
-  
+  validates :phone_number, :fax_number, format: { with: VALID_PHONE_NUMBER_REGEX, message: 'must be valid' }, allow_blank: true
 end
