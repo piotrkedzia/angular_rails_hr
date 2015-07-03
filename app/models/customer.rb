87661bfa25dd46020915b1f70302b9a42041b99f
Piotr Kędzia <piotr@kedzia.eu>
2015-07-04 00:05:11 +0200

    Allow nil discount in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :first_name, :last_name, :email, :address, :zip_code, presence: true
-  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
+  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }, allow_nil: true
   validates :email, format:  { with: VALID_EMAIL_REGEX, message: 'must be valid' }
   validates :phone_number, :fax_number, format: { with: VALID_PHONE_NUMBER_REGEX, message: 'must be valid' }, allow_blank: true
 end
