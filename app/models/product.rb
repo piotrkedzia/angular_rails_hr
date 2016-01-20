58cf22a51c131d77eae3b059bf2cae43fe604c4b
Piotr Kędzia <piotr@kedzia.eu>
2016-01-20 16:14:39 +0000

    Modify validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
-  validates :profit, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }
-  validates :price, numericality: { greater_than_or_equal_to: 0}
+  validates :profit, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }, allow_nil: false
+  validates :price, numericality: { greater_than_or_equal_to: 0}, allow_nil: false
   validates :image_url, format: {with: /\.(png|jpg)\Z/i}
 end
